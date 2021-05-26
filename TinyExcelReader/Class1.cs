216b0d1679b99428ddf1f693dadf0ff777924569
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2021-05-26 15:52:09 +0300

    Add project files.

---
+++ TinyExcelReader/Class1.cs
@@ -0,0 +1,98 @@
+﻿// Install-Package ExcelDataReader -Version 3.6.0 -ProjectName TinyExcelReader
+// Install-Package System.Text.Encoding.CodePages -Version 5.0.0 -ProjectName TinyExcelReader
+
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Text;
+
+using ExcelDataReader;
+
+namespace TinyExcelReader {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Standard Excel Reader
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static class Excel {
+    #region Create
+
+    static Excel() {
+      Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+    }
+
+    #endregion Create
+
+    #region Public
+
+    /// <summary>
+    /// Reads entire MS Excel file
+    /// </summary>
+    /// <param name="filePath">File Path</param>
+    /// <param name="password">Password, if any</param>
+    /// <returns></returns>
+    public static IEnumerable<(string pageName, int pageIndex, string[] row)> ReadLines(
+      string filePath,
+      string password = null) {
+      ExcelReaderConfiguration config = new ExcelReaderConfiguration();
+
+      if (!string.IsNullOrWhiteSpace(password))
+        config.Password = password;
+
+      using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read)) {
+        using (var reader = ExcelReaderFactory.CreateReader(stream, config)) {
+          int page = 0;
+
+          do {
+            while (reader.Read()) {
+              string[] row = new string[reader.RowCount];
+
+              for (int i = 0; i < reader.FieldCount; ++i)
+                row[i] = $"{reader[i]}";
+
+              yield return (reader.Name, page, row);
+            }
+
+            page += 1;
+          } while (reader.NextResult());
+        }
+      }
+    }
+
+    /// <summary>
+    /// Reads entire MS Excel file
+    /// </summary>
+    /// <param name="filePath">File Path</param>
+    /// <param name="password">Password, if any</param>
+    /// <returns></returns>
+    public static IEnumerable<(string pageName, int pageIndex, IDataRecord row)> ReadRecords(
+      string filePath,
+      string password = null) {
+
+      ExcelReaderConfiguration config = new ExcelReaderConfiguration();
+
+      if (!string.IsNullOrWhiteSpace(password))
+        config.Password = password;
+
+      using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read)) {
+        using (var reader = ExcelReaderFactory.CreateReader(stream, config)) {
+          int page = 0;
+
+          do {
+            while (reader.Read())
+              yield return (reader.Name, page, reader);
+
+            page += 1;
+          } while (reader.NextResult());
+        }
+      }
+    }
+
+    #endregion Public
+  }
+
+}
